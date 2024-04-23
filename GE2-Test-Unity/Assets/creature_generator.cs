11ebf2390717d1a8d29e043577a2ee6a6575b2d3
Mickname342 <michele.marchese.andreu@gmail.com>
2024-04-23 11:39:51 +0100

    Made the boid follow an object
    Made the boid follow an objec in a sin wave pattern

--- GE2-Test-Unity/Assets/creature_generator.cs
+++ GE2-Test-Unity/Assets/creature_generator.cs
@@ -6,31 +6,61 @@ public class creature_generator : MonoBehaviour
 {
     public GameObject[] parts;
     public int length = 1;
+    [Range(0,5)]
     public float frequency = 1;
     public float start_angle = 1;
-    public float base_size = 1;
+    public float base_size = 0.5f;
     public float multiplier = 1;
+
+    public float amplitude = 1;
+    public float follow_frequency = 5;
+
     Transform prev_transform;
+    float max_mult = 0;
 
     public GameObject prefab;
+    public GameObject follow;
+
     void Start()
     {
+        float prev_mult;
+        max_mult = Mathf.Sin(frequency + start_angle) + 1;
         parts = new GameObject[length + 1];
         parts[0] = gameObject;
         prev_transform = transform;
+
         for (int i = 0; i < length; i++)
         {
-            prev_transform.transform.localScale = new Vector3(base_size * multiplier, base_size * multiplier, base_size * multiplier);
-            GameObject tail = Instantiate(prefab, new Vector3(0 ,0, prev_transform.position.z - base_size * multiplier), Quaternion.identity);
+            prev_mult = max_mult;
+            max_mult = Mathf.Sin(i * frequency + start_angle) + 1;
+
+            GameObject tail = Instantiate(prefab, new Vector3(0 ,0,
+                prev_transform.position.z - prev_transform.localScale.z/2 - (base_size * multiplier * max_mult)/2),
+                Quaternion.identity);
+
+            prev_transform.transform.localScale = new Vector3(base_size * multiplier * max_mult,
+                base_size * multiplier * max_mult,
+                base_size * multiplier * max_mult);
+
             prev_transform = tail.transform;
             parts[i + 1] = tail;
         }
-        prev_transform.transform.localScale = new Vector3(base_size * multiplier, base_size * multiplier, base_size * multiplier);
+        prev_transform.transform.localScale = new Vector3(base_size * multiplier * max_mult,
+            base_size * multiplier * max_mult,
+            base_size * multiplier * max_mult);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        follow.transform.position = new Vector3(transform.position.x,
+           follow.transform.position.y + Mathf.Sin(Time.time * follow_frequency + start_angle) * amplitude,
+           transform.position.z + 5);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(follow.transform.position, 1);
     }
 }
